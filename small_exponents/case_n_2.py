80ba9d7ad8a8f762beb97aeb4def30e7fa6d7dbc
Angelos Koutsianas <angelos.koutsianas@external.commsquare.com>
2026-08-25 15:46:11 +0300

    Implement case n=2 using S-integral points on elliptic curves.

--- small_exponents/case_n_2.py
+++ small_exponents/case_n_2.py
@@ -1,9 +1,33 @@
 from sage.all import (EllipticCurve, ZZ)
 
+from config import D_BOUND
 
-for d in range(3, 101):
-    if (d - 1)**(1/3) not in ZZ and (d + 1)**(1/3) not in ZZ:
-        Ed = EllipticCurve([0, -d])
-        pts = Ed.integral_points()
-        pts = [P for P in pts if gcd(P[0], P[1]) == 1]
-        print(f"d:{d}, Points: {pts}")
+
+sols = []
+for d in range(2, D_BOUND + 1):
+    if not ZZ(d).is_perfect_power():
+        print(f"d: {d}")
+        S = ZZ(d).prime_factors()
+        for k0 in range(6):
+            E0 = E0min = EllipticCurve([0, -d^k0])
+            if not E0.is_minimal():
+                E0min = E0.minimal_model()
+            phi = E0min.isomorphism_to(E0)
+            for p in phi.u.denominator().prime_factors():
+                if p not in S:
+                    S.append(p)
+            try:
+                pts = E0min.S_integral_points(S=S)
+                for pt in pts:
+                    P0 = phi(pt)
+                    x0 = P0[0].denominator()
+                    y0 = P0[1].denominator()
+                    dk = x0^3 - y0^2
+                    if dk.is_perfect_power():
+                        b, k = dk.perfect_power()
+                        if b == d:
+                            sols.append((x0, y0, d, k))
+            except:
+                print(f"We couldn't compute the S-integral points of {E0} for d={d} and S={S}")
+
+print(f"The solutions are {sols}.")
